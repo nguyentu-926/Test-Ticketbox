7cea1a144bd3af994db8ee78d100ee2750791339
nguyentu-926 <119412483+nguyentu-926@users.noreply.github.com>
2025-07-02 00:44:40 +0700

    last commit

---
+++ Apache Jmeter/jmeter.bat
@@ -0,0 +1,6 @@
+# JMeter generated Header file
+User-Agent	Mozilla/5.0
+Accept	text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+Accept-Encoding	gzip, deflate, br
+Accept-Language	vi,en-US;q=0.9,en;q=0.8
+Connection	keep-alive
